afa345a480f8cff4488daa0c1556981064cbd49b
Rachel Nascimento <rachelle.nascimento@gmail.com>
2021-07-19 15:11:57 +0200

    participants table updated and wf for add event

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'devise'
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'abyme'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,13 @@
 class Event < ApplicationRecord
+  include Abyme::Model
+
   belongs_to :user
-  has_many :participants, dependent: :destroy
+  has_many :participants, inverse_of: :event
   accepts_nested_attributes_for :participants, reject_if: proc { |attributes| attributes['email'].blank? }
 
   validates :event_date, presence: true
   validates :gift_amount, presence: true, numericality: true
   validates_associated :participants
+
+  abymize :participants
 end
